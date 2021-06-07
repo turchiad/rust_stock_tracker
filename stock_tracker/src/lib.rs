67f1b02bc7adba9c35e3d39d0609d04291f6c791
turchiad <turchiad@gmail.com>
2021-06-06 23:47:14 -0400

    Refactor `Config` to use a new enum `Command` in place of the `command: String` field of `Config`

--- stock_tracker/src/lib.rs
+++ stock_tracker/src/lib.rs
@@ -4,10 +4,48 @@
 
 use std::error::Error; // So we may define Box<dyn Error> // To allow for the use of `env::Args` in setting up `Config`
 
+/// The `Command` enum represents the variety of input cases a user could specify.
+pub enum Command {
+    Create,
+    Delete,
+    Login,
+    Logout,
+    Showall,
+    Invalid,
+}
+
+impl Command {
+
+    /// Constructor for the `Command` enum to parse a `String` input
+    pub fn new(s: &str) -> Command {
+        match String::from(s).to_lowercase().as_str() {
+            "c" | "create" => Command::Create,
+            "d" | "delete" => Command::Delete,
+            "li" | "login" => Command::Login,
+            "lo" | "logout" => Command::Logout,
+            "sa" | "showall" => Command::Showall,
+            _ => Command::Invalid,
+        }
+    }
+
+    /// Returns the number of arguments expected after the `Command`
+    pub fn num_args(self) -> i32 {
+        match self {
+            Command::Create => 1,
+            Command::Delete => 1,
+            Command::Login => 1,
+            Command::Logout => 0,
+            Command::Showall => 0,
+            Command::Invalid => 0,
+        }
+    }
+}
+
+/// The `Config` struct represents the CLI input state of a call to this program.
 pub struct Config {
-    // The primary command immediately following the call
-    pub command: String,
-    // The remainder of arguments which may be processed differently depending on the command.
+    /// The primary command immediately following the call
+    pub command: Command,
+    /// The remainder of arguments which may be processed differently depending on the command.
     pub remainder: Vec<String>,
 }
 
@@ -16,7 +54,7 @@ impl Config {
         args.next(); // Discard the first argument
 
         let command = match args.next() {
-            Some(arg) => arg,
+            Some(arg) => Command::new(&arg),
             None => return Err("Didn't get a command string"),
         };
 
